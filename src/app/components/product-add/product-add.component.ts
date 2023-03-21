20572832f57a4fbce9c9bb6a4980bb278abee226
Duhan Turan <dduhanturann@gmail.com>
2023-03-21 18:44:55 +0300

    21 Mart 1. commit

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -30,11 +30,17 @@ this.productAddForm = this.formBuilder.group({
     if(this.productAddForm.valid){
     let productModel = Object.assign({},this.productAddForm.value ) 
     this.productService.add(productModel).subscribe(response=>{
-      console.log(response)
+      
       this.toastrService.success(response.message, "Başarılı")
     }, responseError=>{
-      console.log(responseError.error)
-      this.toastrService.error(responseError)
+      if (responseError.error.Errors.length>0){
+        
+        for (let i = 0; i < responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Doğrulama Hatası");
+        }
+        
+      }
+      
     })
    
     }else{
